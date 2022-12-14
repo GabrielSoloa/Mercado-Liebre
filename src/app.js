4ca866469436ace2d00cde7eab3eacb1e780b648
GabrielSoloa <gabrielsoloa@gmail.com>
2022-12-13 23:41:45 -0300

    Actualizando archivos de package.json y home.html

--- src/app.js
+++ src/app.js
@@ -5,11 +5,15 @@ const publicPath = path.resolve (__dirname , "./public");
 
 app.use (express.static("./public"));
 
-app.listen(3030 , () => {
-    console.log("Corriendo ruta http://www.localhost:3030");
+const port = process.env.PORT || 3001
+app.listen(port , () => {
+    console.log("Corriendo ruta http://www.localhost:" + port);
 });
 
 app.get("/" , (req , res) => {
     let htmlPath = path.resolve(__dirname , ("./views/home.html"));
     res.sendFile(htmlPath);
 });
+
+
+
